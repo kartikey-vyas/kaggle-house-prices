bbbe55b9e9cbef5f26a9f58a1d491cfb8869404e
Kartikey Vyas <kartikeyvyas23@gmail.com>
2021-03-22 22:20:40 +1300

    hacked together a model with literally no exploration

---
+++ src/data/prepare.py
@@ -0,0 +1,23 @@
+# remove certain columns
+cols_to_drop = [
+    "LotFrontage",
+    "Alley",
+    "MasVnrType",
+    "MasVnrArea",
+    "BsmtQual",
+    "BsmtCond",
+    "BsmtExposure",
+    "BsmtFinType1",
+    "BsmtFinType2",
+    "Electrical",
+    "FireplaceQu",
+    "GarageType",
+    "GarageYrBlt",
+    "GarageFinish",
+    "GarageQual",
+    "GarageCond",
+    "PoolQC",
+    "Fence",
+    "MiscFeature",
+]
+
